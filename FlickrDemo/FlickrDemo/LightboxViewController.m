54aaf372c79439ce40efa2c547dfa58390095136
Cristan <cristanz@yahoo-inc.com>
2015-10-02 12:09:08 -0700

    Add app icons for release. add lightbox view for viewing photo detail.

---
+++ FlickrDemo/FlickrDemo/LightboxViewController.m
@@ -0,0 +1,37 @@
+//
+//  LightboxViewController.m
+//  FlickrExplorer
+//
+//  Created by Cristan Zhang on 9/30/15.
+//  Copyright (c) 2015 FastTrack. All rights reserved.
+//
+
+#import "LightboxViewController.h"
+
+@interface LightboxViewController ()
+
+@end
+
+@implementation LightboxViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
